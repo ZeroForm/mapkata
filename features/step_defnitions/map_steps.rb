bd0f1d797ae68804633eb40a974ce8f6c21d212d
Scott Denson <Scott.Denson@gmail.com>
2012-04-30 19:40:46 -0500

    first test

--- features/step_defnitions/map_steps.rb
+++ features/step_defnitions/map_steps.rb
@@ -1,9 +1,11 @@
 require "mapkata"
+begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end 
+
 
 Given /^I have a (\d+)x(\d+) map$/ do |x, y|
   @map = Mapkata.new
-  @map.x = x
-  @map.y = y
+  @map.x = x.to_i
+  @map.y = y.to_i
 end
 
 Given /^I have a map$/ do ||
@@ -13,18 +15,18 @@ Given /^I have a map$/ do ||
 end
 
 When /^I move from (\d+),(\d+) with radius (\d+)$/ do |x, y, radius|
-  pending # express the regexp above with the code you wish you had
+  @map.move_from(x.to_i, y.to_i, radius.to_i)
 end
 
 Then /^square (\d+),(\d+) should be marked$/ do |x, y|
-  pending # express the regexp above with the code you wish you had
+ 	@map.square_marked?(x.to_i,y.to_i).should == true
 end
 
 Then /^no other squares should be marked$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^(\d+) square(.*) should be marked$/ do |count|
-	pending
+Then /^(\d+) square(.*) should be marked$/ do |count,multiplesquares|
+	@map.squares_marked.should  == count.to_i
 end
 
